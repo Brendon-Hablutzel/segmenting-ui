4c62b6d13a9359edca0c89efeb43565ad28b245a
Brendon Hablutzel <booklvrbren@gmail.com>
2025-03-29 20:03:05 -0400

    initial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,66 @@
 import Link from 'next/link';
 import UploadCloudIcon from '../svg/UploadCloudIcon';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="max-h-[100vh] max-w-[100vw] bg-bg-dark grid grid-rows-[20vh_20vh_20vh] py-16 px-5 gap-10 font-[family-name:var(--font-ibm-sans)]">
-      <div className="flex flex-col justify-end">
-        <div className="flex justify-center">
-          <UploadCloudIcon strokeWidth="4" pathClassName="stroke-text-light" />
-        </div>
-      </div>
-      <div className="flex flex-col justify-center gap-8">
-        <div className="flex items-center justify-center">
-          <h1 className="text-text-light text-7xl w-full text-center">
-            Seg UI
-          </h1>
-        </div>
-        <div className="flex items-center justify-center">
-          <h1 className="text-text-light text-xl text-center">
-            A platform for generalized image processing and analysis
-          </h1>
-        </div>
-      </div>
-      <div className="flex justify-center p-2 h-fit">
-        <div className="flex flex-col gap-5">
-          <div className="flex justify-center">
+    <div className="text-text-light h-screen">
+      <div className="sticky top-0 z-[1000] flex justify-between items-center px-8 py-9 bg-[#061006] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] h-14">
+        <div className="flex justify-start items-center gap-7 select-none">
+          <div className="flex justify-between items-center gap-4">
+            <div>
+              <UploadCloudIcon
+                strokeWidth="5"
+                pathClassName="stroke-text-light"
+                svgClassName="h-10 w-10"
+              />
+            </div>
+            <div className="text-2xl whitespace-nowrap">Seg UI</div>
+          </div>
+          <div className="text-lg max-sm:hidden">
             <Link
-              href="/login"
-              className="flex justify-center items-center text-xl rounded-3xl bg-text-light text-text-dark hover:cursor-pointer transition ease-out duration-150 w-[25rem] max-w-[80vw] h-[3rem]"
+              href="https://github.com/Brendon-Hablutzel/segmenting-ui"
+              target="_blank"
             >
-              Log In
+              source
             </Link>
           </div>
-          <div className="flex justify-center">
-            <Link
-              href="/signup"
-              className="flex justify-center items-center text-xl border-[0.5px] border-text-light text-text-light rounded-3xl transition ease-out duration-150 w-[25rem] max-w-[80vw] h-[3rem]"
-            >
-              Sign Up
-            </Link>
+        </div>
+        <div className="flex justify-end items-center gap-5">
+          <div className="flex justify-center items-center whitespace-nowrap py-[1.1rem] px-6 select-none transition ease-out duration-150 rounded-2xl h-[2rem] text-lg border-[0.5px] border-text-light text-text-light hover:cursor-pointer">
+            <Link href="/login">Log In</Link>
           </div>
+          <div className="max-sm:hidden flex justify-center items-center whitespace-nowrap py-[1.1rem] px-6 select-none transition ease-out duration-150 rounded-2xl h-[2rem] text-lg bg-text-light text-text-dark hover:cursor-pointer hover:brightness-75">
+            <Link href="/signup">Sign Up</Link>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 flex flex-col items-center justify-center gap-5 md:gap-6 animate-fadeIn">
+        <div className="text-4xl sm:text-5xl md:text-6xl leading-tight font-bold text-center w-[90%] sm:w-[85%] md:w-[80%] lg:w-[80%] xl:w-[70%]">
+          Generalized image transformation and analysis
+        </div>
+        <div className="text-base sm:text-lg md:text-xl text-center w-[95%] sm:w-[85%] md:w-[75%] lg:w-[55%] xl:w-[45%]">
+          Just upload an image, select from a variety of modules, and processing
+          will occur in seconds
+        </div>
+        <div className="text-center bg-text-light text-bg-dark py-[0.3rem] px-14 rounded-2xl text-xl select-none transition ease-out duration-150 hover:cursor-pointer hover:brightness-75">
+          <Link href="/signup">Create an account</Link>
+        </div>
+        <div className="relative h-[65vw] sm:h-[370px] md:h-[400px] mt-3 max-w-[600px] w-[90%]">
+          <Image
+            className="object-cover max-w-[90%] animate-fromLeft absolute top-0 left-0 shadow-lg rounded-lg border-[1px] border-text-light/10"
+            alt="screenshot of dashboard"
+            src={'/dashboard-screenshot.png'}
+            width={500}
+            height={100}
+          />
+          <Image
+            className="w-[80%] max-w-[350px] object-cover animate-fromRight absolute bottom-0 right-0 shadow-lg rounded-lg border-[1px] border-text-light/10"
+            alt="screenshot of add jobs page"
+            src={'/add-job-screenshot.png'}
+            width={350}
+            height={100}
+          />
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,34 @@ export default {
         'bg-primary': '#2D8147',
         'bg-card': '#0B130B',
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '10%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fromLeft: {
+          '0%': {
+            left: '-75',
+          },
+          '100%': {
+            left: '0',
+          },
+        },
+        fromRight: {
+          '0%': {
+            right: '-50',
+          },
+          '100%': {
+            right: '0',
+          },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1.5s forwards',
+        fromLeft: 'fromLeft 1s forwards',
+        fromRight: 'fromRight 1s forwards',
+      },
     },
   },
   plugins: [],
